f1965baa8f6c06ae9f85026d9e9e0c9add116c19
Matheus Moreira <mvrmoreira@gmail.com>
2016-09-20 16:17:00 -0300

    Writes the skeleton

---
+++ OneSignal.CSharp.SDK/IOneSignalClient.cs
@@ -0,0 +1,17 @@
+﻿using OneSignal.CSharp.SDK.Resources.Notifications;
+using OneSignal.CSharp.SDK.Resources.Devices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneSignal.CSharp.SDK
+{
+    public interface IOneSignalClient
+    {
+        IDevicesResource Devices { get; }
+
+        INotificationsResource Notifications { get; }
+    }
+}
