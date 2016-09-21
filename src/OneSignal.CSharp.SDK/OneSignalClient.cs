b47a5cd0348d941d5e1dfa8cc5567b2f2851c254
Matheus Moreira <mvrmoreira@gmail.com>
2016-09-21 15:16:52 -0300

    Try to fix appveyor

--- src/OneSignal.CSharp.SDK/OneSignalClient.cs
+++ src/OneSignal.CSharp.SDK/OneSignalClient.cs
@@ -5,13 +5,14 @@ namespace OneSignal.CSharp.SDK
 {
     public class OneSignalClient : IOneSignalClient
     {
-        public IDevicesResource Devices { get; }
+        public IDevicesResource Devices { get; private set; }
 
-        public INotificationsResource Notifications { get; }
+        public INotificationsResource Notifications { get; private set; }
 
         public OneSignalClient(string apiKey, string apiUri = "https://onesignal.com/api/v1")
         {
             this.Devices = new DevicesResource(apiKey, apiUri);
+            this.Notifications = new NotificationsResource(apiKey, apiUri);
         }
     }
 }
