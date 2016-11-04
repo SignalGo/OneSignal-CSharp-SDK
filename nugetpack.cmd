bf68fd622372a08e02001ac13bdd60bd95e8dd62
Matheus Moreira <mvrmoreira@gmail.com>
2016-11-04 15:04:28 -0200

    Upgrade version

--- nugetpack.cmd
+++ nugetpack.cmd
@@ -1,3 +1,5 @@
+msbuild src\OneSignal.CSharp.SDK.sln /verbosity:minimal /p:Configuration=Release
+
 copy .\src\OneSignal.CSharp.SDK\bin\Release\OneSignal.CSharp.SDK.dll .\build\net45\OneSignal.CSharp.SDK.dll /Y
 
 cd build
